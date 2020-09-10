28ac00eebe0b2fa623105a498f05567a492842d0
Josue <josrangel@hotmail.com>
2020-09-10 18:24:57 -0500

    Se agrega la funcionalidad de busqueda con el archivo wikifunc.js

---
+++ assets/js/wikifunc.js
@@ -0,0 +1,64 @@
+function busca(){
+	let cadena=document.getElementById("searchterm").value;
+  let resultados=document.getElementById("resultados");
+  if(cadena.length<2){
+    return false;
+  }
+
+	var flickerAPI = "https://es.wikipedia.org/w/api.php";
+  	$.getJSON( flickerAPI, {
+    	action: 'query',
+    	format: 'json',
+    	list: 'search',
+    	srsearch: cadena,
+    	origin: '*'
+  	})
+    
+    .done(function( data ) {
+      //let jsonRespuesta=JSON.parse(data);
+      let jSearch=data.query.search;
+      resultados.innerHTML="";
+
+      let tabla = "<table><tr><th>Numero de Pagina</th><th>Titulo</th><th>Resumen</th></tr>";
+    	console.log(data);
+      $.each( jSearch, function( i, item ) {
+        //$( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        let pageId=item.pageid;
+        let title=item.title;
+        let snippet=item.snippet;
+        tabla += "<tr><td>"+pageId+"</td><td>"+title+"</td><td>"+snippet+"</td></tr>";
+      });/**/
+      tabla += "</table>";
+      resultados.innerHTML= tabla ; 
+    })
+    .fail(function() {
+    console.log( "error" );
+    });
+}
+
+/*
+function busca(){
+	let cadena=document.getElementById("buscador").value;
+	
+	let parametrosBody= {
+        					action: 'query',
+        					format: 'json',
+        					list: 'search',
+					        //utf8: '1',
+        					srsearch: cadena,
+        					origin: '*'
+      					};
+
+	fetch('https://es.wikipedia.org/w/api.php',{
+		method: 'GET',
+		params: parametrosBody
+	})
+  		
+  	.then(function(myJson) {
+    	console.log(myJson);
+  	})
+	.catch(function(error) {
+  		console.log('Hubo un problema con la petición Fetch:' + error.message);
+	});
+}
+ */
